e71c8746bbed8d34d1fb18592d48922b9313511b
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2024-09-03 15:06:21 -0500

    Nothing

--- src/components/Testimonio/Testimonio.js
+++ src/components/Testimonio/Testimonio.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Testimonio.css";
 
-
 function Testimonio(props) {
   return(
     <div className="contenedor__testimonio">
       <img
         className="testimonio__imagen"
         src={require(`../../img/testimonio-${props.imagen}.png`)}
-        alt="Foto de Emma"
+        alt='Foto de Emma'
       />
       <div className="testimonio__texto">
         <p className="texto__nombre">{props.nombre} en {props.pais}</p>
